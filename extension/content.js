0f85b39736b49f021ecc5c22ac6963340f98c976
dhinesh0-bi <kdhinesh2005@gmail.com>
2026-03-03 19:25:18 +0530

    hosting changes

--- extension/content.js
+++ extension/content.js
@@ -40,7 +40,7 @@ async function scanReviews() {
         let rating = 5; // Default assumption
         
         try {
-            let response = await fetch("http://127.0.0.1:8000/analyze", {
+            let response = await fetch("https://ai-fake-review-detection-system.onrender.com/analyze", { 
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ text: reviewText, rating: rating })
