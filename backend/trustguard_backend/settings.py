b2a2069f6b91aea8f8dfcf7b3ff04db919da3945
dhinesh0-bi <kdhinesh2005@gmail.com>
2026-04-05 16:02:41 +0530

    Removed

--- backend/trustguard_backend/settings.py
+++ backend/trustguard_backend/settings.py
@@ -45,9 +45,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # <-- Add this line RIGHT HERE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
